4e8b50a4a7707a21916e7bdb8ac8ddd200fa1b3f
papanapaepic <104739916+papanapaepic@users.noreply.github.com>
2022-05-05 17:23:38 +0300

    new site

---
+++ discriminant.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ДИСКРИМИНАНТ</title>
+    <link rel="stylesheet" href="C:/OpenServer/domains/sterter/styles.css">
+</head>
+<body>
+<audio src="music.mp3"controls></audio>
+    <center>Решение дискриминанта</center>
+<header>
+    <nav>
+<a href="#">ссылка  №1</a>
+<a href="#">ссылка  №2</a>
+<a href="#">ссылка  №3</a>
+<a href="#">ссылка  №4</a>
+<a href="#">ссылка  №5</a>
+
+</nav>
+</header>
+<section>
+   
+</section>
+<hr>
+<ul>
+    <b>
+<li> записать а</li> 
+<li>записать b</li>
+</b>
+<li>записать c</li>
+ <img src="img/1.jpg" width="250">  
+
+</ul>
+
+
+    <?php
+    
+
+function discriminant ($a, $b,$c)
+{
+
+ echo 'D=' .$resolt= $b*$b-4*$a*$c ;
+ echo '<br>';
+ if ($resolt<0) {
+     
+echo 'корней нет ';
+}
+else
+echo 'есть два корня' ;
+
+
+}
+
+
+echo  discriminant(a:3,b:-14,c:-5) 
+?>
+
+
+</body>
+</html>
+<?php
+
+
+
